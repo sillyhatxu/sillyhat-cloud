0aaf7429961c3ce5270aa89fd5fc7fb1f43e8e40
xushikuan <xushikuan1@163.com>
2017-11-17 15:06:04 +0800

    swagger h2 bootstrap

--- web-app/src/main/java/com/sillyhat/cloud/webapp/config/SwaggerConfig.java
+++ web-app/src/main/java/com/sillyhat/cloud/webapp/config/SwaggerConfig.java
@@ -8,9 +8,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
- * Created by deadlock on 22/8/17.
- */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {

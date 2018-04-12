0615d625546207ad0372d6a07d762be2a35a9706
xushikuan <xushikuan1@163.com>
2018-04-12 14:47:52 +0800

    scheduler

--- customer/src/main/java/com/sillyhat/cloud/customer/task/SchedulerService.java
+++ customer/src/main/java/com/sillyhat/cloud/customer/task/SchedulerService.java
@@ -1,5 +1,6 @@
 package com.sillyhat.cloud.customer.task;
 
+import com.sillyhat.cloud.customer.model.Customer;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 @Slf4j
 @Service
@@ -16,7 +18,25 @@ public class SchedulerService {
     private static SimpleDateFormat sdfTime = new SimpleDateFormat( "yyyyMMddHHmmss");
 
     @Scheduled(cron="0/2 * * * * ? ")
-    public void scheduleTestLog(){
-        log.info("schedule test log. time : {}",sdfTime.format(new Date()));
+    public void scheduleTestInfoLog(){
+        log.info("schedule test info log. time : {}",sdfTime.format(new Date()));
+    }
+
+    @Scheduled(cron="0/8 * * * * ? ")
+    public void scheduleTestDTOInfoLog(){
+        Random random = new Random();
+        Long id = random.nextLong();
+        log.info("schedule test dto info log. time : {} ; dto : {}",sdfTime.format(new Date()),Customer.builder().id(id).email("test@gmail.com").build());
+    }
+
+    @Scheduled(cron="0 0/3 * * * ? ")
+    public void scheduleTestErrorLog(){
+        try {
+            Long.parseLong("xxxxx");
+        } catch (Exception e){
+            log.info("schedule test error log. time : {}",sdfTime.format(new Date()));
+            log.error("format number error",e);
+        }
+
     }
 }
